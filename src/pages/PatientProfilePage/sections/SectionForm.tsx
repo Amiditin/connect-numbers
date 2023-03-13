83f8635aa8c424a7c80160bb353cdeb9977a43a0
Dmitry Fomin <dimafomin02@gmail.com>
2023-03-13 15:29:55 +0300

    Добавлена верстка удаления пациента

--- src/pages/PatientProfilePage/sections/SectionForm.tsx
+++ src/pages/PatientProfilePage/sections/SectionForm.tsx
@@ -1,34 +1,47 @@
 import { useState } from 'react';
-import { Button, message, Typography } from 'antd';
-import { EditOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+import { Button, message, Popconfirm, Space, Typography } from 'antd';
+import { DeleteOutlined, EditOutlined, QuestionCircleOutlined } from '@ant-design/icons';
 
 import { FormPatient, type IFormPatientValues } from '@/components';
 import { devDataUser } from '../constants';
 
 import styles from '../PatientProfilePage.module.scss';
+import { routes } from '@/router';
 
 const { Title } = Typography;
 
 export const SectionForm: React.FC = () => {
   const [messageApi, contextHolder] = message.useMessage();
   const [isEdited, setIsEdited] = useState(false);
+  const navigate = useNavigate();
 
-  const success = () => {
+  const success = (loadingText: string, successText: string) => {
     messageApi
       .open({
         type: 'loading',
-        content: 'Сохраняем изменения..',
+        content: loadingText,
         duration: 2.5,
       })
       .then(() => {
-        message.success('Изменения успешно сохранены!', 2.5);
+        message.success(successText, 2.5);
         setIsEdited(false);
       });
   };
 
   const handleEditPatient = (values: IFormPatientValues) => {
     console.log(values);
-    success();
+    success('Сохраняем изменения...', 'Изменения успешно сохранены!');
+  };
+
+  const handleDeletePatient = () => {
+    success('Удаляем профиль...', 'Пациент успешно удалён!');
+
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(null), 3000);
+    }).then(() => {
+      navigate(routes.patients.path);
+    });
   };
 
   return (
@@ -36,14 +49,32 @@ export const SectionForm: React.FC = () => {
       {contextHolder}
       <div className={styles.title_box}>
         <Title level={3}>Персональные данные:</Title>
-        <Button
-          className={styles.btn}
-          type="primary"
-          disabled={isEdited}
-          icon={<EditOutlined />}
-          onClick={() => setIsEdited(true)}>
-          Редактировать
-        </Button>
+        <Space size="middle">
+          <Button
+            className={styles.btn}
+            type="primary"
+            disabled={isEdited}
+            icon={<EditOutlined />}
+            onClick={() => setIsEdited(true)}>
+            Редактировать
+          </Button>
+          <Popconfirm
+            title="Удалить профиль"
+            description="Вы уверены, что хотите удалить этого пациента?"
+            okText="Удалить"
+            okType="danger"
+            icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+            onConfirm={handleDeletePatient}>
+            <Button
+              className={styles.btn}
+              danger
+              type="primary"
+              disabled={isEdited}
+              icon={<DeleteOutlined />}>
+              Удалить
+            </Button>
+          </Popconfirm>
+        </Space>
       </div>
       <FormPatient
         initialValues={{
