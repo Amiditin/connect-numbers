e772e8e9476ae1b24e85d3124dd59f05ade836ac
Dmitry Fomin <dimafomin02@gmail.com>
2023-03-28 04:14:58 +0300

    feat(utils): Добавлены функции getRandomNumber getMatrixItems

---
+++ src/shared/utils/getMatrixItems/types.ts
@@ -0,0 +1,25 @@
+import type { IMatrixOptions } from '@/shared/constants';
+
+export interface IMatrixItem {
+  active: boolean;
+  coordX: number;
+  coordY: number;
+  number: number;
+  text: string;
+}
+
+export interface IGetMatrixItemsOptions {
+  withLetters?: boolean;
+  numberItems?: number;
+  matrixOptions?: IMatrixOptions;
+}
+
+export interface IInterval {
+  min: number;
+  max: number;
+}
+
+export interface IIntervalOptions {
+  width: IInterval;
+  height: IInterval;
+}
